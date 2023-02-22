75ec8bee50cac220a19548d022acc7bcf8d308c4
Lucas Farias(PC CASA) <lucasfariaszanona@gmail.com>
2023-02-22 19:58:19 -0300

    Adicionando modulo 1 ao 3

---
+++ exercises/CAP3EX01/E03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Entre com o seu nome completo:");
+        string nome1 = Console.ReadLine();
+
+        Console.WriteLine("Quantos quartos tem na sua casa:");
+        int numQuarto = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre com o preço de um produto:");
+        double valorProd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Entre seu ultimo nome, idade e altura:");
+
+        String[] dados = Console.ReadLine().Split(" ");
+
+        string nome2 = dados[0];
+        int idade = int.Parse(dados[1]);
+        double altura = double.Parse(dados[2], CultureInfo.InvariantCulture);
+
+        Console.WriteLine(nome1);
+        Console.WriteLine(numQuarto);
+        Console.WriteLine(valorProd.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine(nome2);
+        Console.WriteLine(idade);
+        Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+}
---
+++ exercises/CAP3EX03/CAP3EX2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a largura: ");
+        double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Digite o comprimento: ");
+        double comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Digite o valor por metro: ");
+        double valorMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double area = largura * comprimento;
+
+        double precoArea = area * valorMetro;
+
+        Console.WriteLine("A area do terrono é: " + area.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("E seu valor é: " + precoArea.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+    }
+
+}
