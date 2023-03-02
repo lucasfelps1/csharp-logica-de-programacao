62fb906dcb01e3dd7c0ddd5a69873b2905d11621
Zanona Lucas <Lucas.Zanona@andritz.com>
2023-03-02 14:55:30 -0300

    Iniciando estruturas de repetição

---
+++ exercises/CAP4EX02/CAP4EX02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b, c;
+
+        String[] vet = Console.ReadLine().Split(' ');
+
+        a = int.Parse(vet[0]);
+        b = int.Parse(vet[1]);
+        c = int.Parse(vet[2]);
+
+        if (a < b && a < c) {
+            Console.WriteLine("MENOR = " + a);
+        } else if (b < a && b < c) {
+            Console.WriteLine("MENOR = " + b);
+        } else { 
+            Console.WriteLine("MENOR = " + c);
+        }
+    }
+}
--- exercises/URI/URI/Program.cs
+++ exercises/URI/URI/Program.cs
@@ -1,15 +1,55 @@
-﻿internal class Program
+﻿using System.Globalization;
+
+
+namespace uri1046
 {
-    private static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
 
-        string[] vetor = Console.ReadLine().Split(' '); 
+            double n1, n2, n3, n4, media, notaExame, mediaExame;
 
-        double A = double.Parse(vetor[0]);
-        double B = double.Parse(vetor[1]);
-        double C = double.Parse(vetor[2]);
+            String[] vet = Console.ReadLine().Split(' ');
 
-        Console.WriteLine(A + B + C);
+            n1 = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            n2 = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            n3 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            n4 = double.Parse(vet[3], CultureInfo.InvariantCulture);
 
+            media = (n1 + n2 + n3 + n4) / 4.0;
+
+            if (media >= 7.0) {
+                Console.WriteLine("Media: " + media);
+                Console.WriteLine("Aluno aprovado.");
+            }
+            else if (media < 5.0) {
+                Console.WriteLine("Media: " + media);
+                Console.WriteLine("Aluno reprovado");
+            }
+            else if (media >= 5.0 && media <= 6.9) {
+                Console.WriteLine("Media: " + media);
+                Console.WriteLine("Aluno em exame");
+
+                notaExame = double.Parse(Console.ReadLine());
+                Console.WriteLine("Nota do exame: " + notaExame);
+
+                mediaExame = (media + notaExame) / 2;
+
+                if (mediaExame > 5.0) { 
+                    Console.WriteLine("Aluno aprovado.");
+                    Console.WriteLine("Media final: " + mediaExame);
+                } 
+                else {
+                    Console.WriteLine("Aluno reprovado.");
+                    Console.WriteLine("Media final: " + mediaExame);
+                }
+                
+            }
+            
+            
+
+         }
+       
     }
 }
