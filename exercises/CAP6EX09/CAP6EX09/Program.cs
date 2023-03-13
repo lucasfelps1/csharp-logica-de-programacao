ffb391ac1586e327018a88cc102a8272d203bea4
Zanona Lucas <Lucas.Zanona@andritz.com>
2023-03-13 14:45:57 -0300

    teste

---
+++ exercises/CAP6EX09/CAP6EX09/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Net.Http.Headers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int N;
+        double[] elementos;
+        double soma;
+
+        N = int.Parse(Console.ReadLine());
+        elementos = new double[N];
+
+        string[] s = Console.ReadLine().Split(' ');
+
+        soma = 0.0;
+        for (int i = 0; i < N; i++)
+        {
+            elementos[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+            soma = soma + elementos[i];
+        }
+
+        double mediaA = soma / N;
+
+        Console.WriteLine(mediaA.ToString("F3", CultureInfo.InvariantCulture));
+
+        for (int i = 0; i < N; i++)
+        {
+            if (elementos[i] < mediaA)
+            {
+                Console.WriteLine(elementos[i]);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ exercises/CAP6EX10/CAP6EX10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int N, count;
+        int[] nums;
+        double soma, mediaA;
+
+        N = int.Parse(Console.ReadLine());
+        nums = new int[N];
+
+        string[] s = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < N; i++)
+        {
+            nums[i] = int.Parse(s[i]);
+        }
+
+        soma = 0.0;
+        count = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                soma = soma + (double) nums[i];
+                count = count + 1;
+            }
+
+        }
+
+        mediaA = soma / (double) count;
+
+        Console.WriteLine(mediaA.ToString("F1", CultureInfo.InvariantCulture));
+
+
+    }
+}
